8d1a9c63a9013bcd5316d9232fb2ca12cb6828d2
Olaf Mihari <olafnjenga@gmail.com>
2023-03-24 07:47:17 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    const form = document.querySelector('#create-task-form');
+  const taskList = document.querySelector('#tasks');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // prevent default form submission behavior
+
+    // Get the task description input value
+    const taskInput = document.querySelector('#new-task-description');
+    const taskDescription = taskInput.value.trim();
+
+    // Add the new task to the task list
+    const newTask = document.createElement('li');
+    newTask.innerText = taskDescription;
+    taskList.appendChild(newTask);
+
+    // Clear the input field
+    taskInput.value = '';
+  });
+
+  taskList.addEventListener('click', function(event) {
+    if (event.target.tagName === 'LI') {
+      event.target.remove();
+    }
+  });
 });
